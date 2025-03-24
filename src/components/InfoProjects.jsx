4951191b97f5ff1a30c402efc13a885964b0812e
Adriano Barros <adrianobarros101@gmail.com>
2025-03-24 17:11:48 -0300

    Implement new portfolio section

---
+++ src/components/InfoProjects.jsx
@@ -0,0 +1,164 @@
+import styled from "styled-components";
+
+const Information = styled.div`
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 25px 25px 0px 25px;
+    font-family: "Readex Pro", serif;
+    color: #9696A6;
+
+    img {
+        width: 415px;
+        height: 245px;
+    }
+
+    h4 {
+        align-self: start;
+        margin-block: 20px;
+    }
+
+    p {
+        font-size: 11px;
+        font-weight: bold;
+        color: rgba(150, 150, 166, 0.5);
+    }
+`;
+
+// Project Button
+const ProjectButton = styled.button`
+    display: flex;
+    width: 140px;
+    height: 40px;
+    color: white;
+    background-color: #1C2027;
+    border: none;
+    border-radius: 5px;
+    font-size: 11px;
+    font-weight: bold;
+    position: relative;
+    transition: border 0.2s ease;
+    z-index: 1;
+    text-align: start;
+    padding: 11px;
+    gap: 18px;
+
+    img {
+        width: 18px;
+        height: 18px;
+    }
+
+    &::before {
+        content: '';
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background: linear-gradient(90deg, #2D2E4D, #696BB3);
+        transform: scaleX(0);
+        transform-origin: left;
+        transition: transform 0.3s ease-in-out;
+        z-index: -1;
+        border-radius: 5px;
+    }
+
+    &:hover::before,
+    &:focus::before {
+        transform: scaleX(1);
+    }
+`;
+
+// Details Button
+const DetailsButton = styled.button`
+    display: flex;
+    width: 107px;
+    height: 40px;
+    background-color: #1C2027;
+    color: #9696A6;
+    border: none;
+    border-radius: 5px;
+    font-size: 12px;
+    font-weight: bold;
+    font-family: "Readex Pro", serif;
+    padding: 12px;
+    gap: 13px;
+
+    img {
+        width: 15px;
+        height: 15px;
+        margin-top: 1px;
+    }
+`;
+
+export function InfoProjects ({ nameProject }) {
+
+    if(nameProject === "healthyClinics") {
+        return (
+            <Information>
+
+                <img src="./images/HealthyClinicsThumb.png" alt="HealthyClinics"/>
+                <h4>Healthy Clinics</h4>
+                <p>O software é uma plataforma de saúde que conecta pacientes, médicos e responsáveis por clínicas, facilitando o agendamento de consultas, a gestão de clínicas e o registro de informações médicas.</p>
+                <div className="col-12 d-flex justify-content-between">
+                    <ProjectButton>
+                        Projeto Github
+                        <img src="./images/LinkIcon.png" alt="Link Icon" />
+                    </ProjectButton>
+                    <DetailsButton>
+                        Detalhes
+                        <img src="./images/DetailsArrow.png" alt="Arrow Icon" />
+                    </DetailsButton>
+                </div>
+
+            </Information>
+        );
+    }
+
+    if(nameProject === "brainTumorSystem") {
+        return (
+            <Information>
+
+                <img src="./images/BrainTumorSystemThumb.png" alt="HealthyClinics"/>
+                <h4>Brain Tumor System</h4>
+                <p>O software é uma plataforma de saúde que conecta pacientes, médicos e responsáveis por clínicas, facilitando o agendamento de consultas, a gestão de clínicas e o registro de informações médicas.</p>
+                <div className="col-12 d-flex justify-content-between">
+                    <ProjectButton>
+                        Projeto Github
+                        <img src="./images/LinkIcon.png" alt="Link Icon" />
+                    </ProjectButton>
+                    <DetailsButton>
+                        Detalhes
+                        <img src="./images/DetailsArrow.png" alt="Arrow Icon" />
+                    </DetailsButton>
+                </div>
+
+            </Information>
+        );
+    }
+
+    if(nameProject === "webPortfolio") {
+        return (
+            <Information>
+
+                <img src="./images/WebPortfolioThumb.png" alt="HealthyClinics"/>
+                <h4>Web Portfolio</h4>
+                <p>O software é uma plataforma de saúde que conecta pacientes, médicos e responsáveis por clínicas, facilitando o agendamento de consultas, a gestão de clínicas e o registro de informações médicas.</p>
+                <div className="col-12 d-flex justify-content-between">
+                    <ProjectButton>
+                        Projeto Github
+                        <img src="./images/LinkIcon.png" alt="Link Icon" />
+                    </ProjectButton>
+                    <DetailsButton>
+                        Detalhes
+                        <img src="./images/DetailsArrow.png" alt="Arrow Icon" />
+                    </DetailsButton>
+                </div>
+                
+            </Information>
+        );
+    }
+
+}
--- src/pages/elements/Portfolio.jsx
+++ src/pages/elements/Portfolio.jsx
@@ -1,10 +1,11 @@
 import PropTypes from "prop-types";
 import styled from "styled-components";
-import { SliderPortfolio } from "../../components/Slider";
+import { Projects } from "../../components/Projects";
 import "../../styles/Global.css";
 
 /* Decorative lines */
 const Lines = styled.div`
+    padding: 0;
     width: 64px;
     height: 64px;
     border-top: 2px solid #9696A6;
@@ -14,21 +15,20 @@ const Lines = styled.div`
 export function Portfolio(props) {
 
     return (
-        <section className="container-fluid pe-lg-0 ps-lg-5" id={props.id}>
+        <section className="container-fluid pe-lg-0" id={props.id}>
 
-            <div className="row d-none d-lg-block">
-                <Lines></Lines>
+            <div className="row d-none d-lg-flex position-relative">
+                <Lines />
+                <Lines style={{ position: "absolute", right: "12px", transform: "rotate(90deg)" }}/>
             </div>
 
-            <header>
-                <h2 className="row d-flex justify-content-center align-items-center waveColor">Portfolio</h2>
-            </header>
-            
-            {/* Component that will generate the slider */}
-            <SliderPortfolio />
+            <h2 className="row d-flex justify-content-center align-items-center waveColor">Portfolio</h2>
+
+            <Projects />
                 
-            <div className="row d-none d-lg-block" style={{marginTop: "90px"}}>
-                <Lines style={{transform: "rotate(270deg)"}}></Lines>
+            <div className="row d-none d-lg-block position-relative" style={{marginTop: "71px"}}>
+                <Lines style={{ marginTop: "12px", transform: "rotate(270deg)" }} />
+                <Lines style={{ position: "absolute", right: "12px", transform: "rotate(180deg)" }}/>
             </div>
 
         </section>
